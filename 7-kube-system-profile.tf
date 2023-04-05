308f16f4a8cc528ef790113eeff5dac2f3458efe
siakan1107 <121685349+siakan1107@users.noreply.github.com>
2023-04-05 18:42:47 +0900

    Update 7-kube-system-profile.tf

--- 7-kube-system-profile.tf
+++ 7-kube-system-profile.tf
@@ -34,3 +34,20 @@ resource "aws_eks_fargate_profile" "kube-system" {
     namespace = "kube-system"
   }
 }
+
+resource "aws_eks_fargate_profile" "staging" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "staging"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.private-us-east-1a.id,
+    aws_subnet.private-us-east-1b.id
+  ]
+
+  selector {
+    namespace = "staging"
+  }
+}
