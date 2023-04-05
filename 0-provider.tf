39489e86c5d5ed205d80871968e6376494018c31
siakan1107 <121685349+siakan1107@users.noreply.github.com>
2023-04-05 12:04:55 +0900

    Add files via upload
    basic env configure

---
+++ 0-provider.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = eu-west-2"
+}
+
+variable "cluster_name" {
+  default = "demo"
+}
+
+variable "cluster_version" {
+  default = "1.22"
+}
+
+terraform {
+  required_providers {
+    aws = {
+	  source = "hashicorp/aws"
+	  version = "~>4.0"
+	}
+  }
+}
+
