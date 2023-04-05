4934cd33b74dce66eacade83ac6ee14a47a38650
siakan1107 <121685349+siakan1107@users.noreply.github.com>
2023-04-05 17:51:54 +0900

    Update 7-kube-system-profile.tf

--- 7-kube-system-profile.tf
+++ 7-kube-system-profile.tf
@@ -15,13 +15,13 @@ resource "aws_iam_role" "eks-fargate-kb-profile" {
 
 resource "aws_iam_role_policy_attachment" "eks-fargate-kb-profile" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-  role       = aws_iam_role.eks-fargate-profile.name
+  role       = aws_iam_role.eks-fargate-kb-profile.name
 }
 
 resource "aws_eks_fargate_profile" "kube-system" {
   cluster_name           = aws_eks_cluster.cluster-kb.name
   fargate_profile_name   = "kube-system"
-  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+  pod_execution_role_arn = aws_iam_role.eks-fargate-kb-profile.arn
 
   # These subnets must have the following resource tag:
   # kubernetes.io/cluster/<CLUSTER_NAME>.
